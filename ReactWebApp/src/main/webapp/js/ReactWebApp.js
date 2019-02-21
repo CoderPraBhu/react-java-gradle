e63d859fb2b36b3ffee136999243bbdffe853d53
Prashant_Bhuruk <Prashant_Bhuruk@HDL05338.persistent.co.in>
2019-02-20 23:50:05 -0800

    Webpack babel and react

---
+++ ReactWebApp/src/main/webapp/js/ReactWebApp.js
@@ -0,0 +1,294 @@
+'use strict';
+const e = React.createElement;
+//Like button start
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+const user = {
+  firstName: 'Sanskruti',
+  lastName: 'Kolpe'
+};
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+	const element = (<button onClick={() => this.setState({ liked: true })}>
+			Like     Hello, {formatName(user)}!
+		</button>);
+	return (element);
+  }
+}
+//Like button end
+//Clock start
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {date: new Date()};
+	}
+	componentDidMount() { 
+		this.timerId = setInterval( () => this.tick(), 1000);
+	}
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
+	tick() {
+		this.setState({date: new Date()});
+	}
+	render(){
+		const element = (
+			<div>
+				<h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+			</div>
+		);
+		return(element);	
+	}
+}
+//Clock end
+//Click and log start
+function ActionLink() {
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
+  return (
+    <a href="#" onClick={handleClick}>
+      Clicking here will log a message in console log but not reload the page
+    </a>
+  );
+}
+//Click and log end 
+//Toggle start
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+//Toggle end
+//Login logout start
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+//Login logout end
+//Name form start
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+//Name form end
+//Essay form start
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+//Essay form end
+//Flavor form start
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconut'};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+//Flavor form end
+//Reservation start
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
+//Reservation end
+function App() {
+  return (
+    <div>
+			<LikeButton />
+			<Clock />
+			<ActionLink />
+			<p><Toggle /></p>
+			<LoginControl />
+			<NameForm />
+			<EssayForm />
+			<FlavorForm />
+			<Reservation />
+    </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
--- ReactWebApp/webpack.config.js
+++ ReactWebApp/webpack.config.js
@@ -2,9 +2,23 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',	
-  entry: './src/main/webapp/js/index.js',
+  entry: {
+//	    index: './src/main/webapp/js/index.js',
+		webapp: './src/main/webapp/js/ReactWebApp.js'	
+  },
   output: {
     path: path.resolve(__dirname, './build/js'),
-    filename: 'webappbundle.js'
-  }
+    filename: '[name].min.js'
+  },
+  module: {
+	    rules: [
+    	  { test: /\.css$/, use: 'css-loader' },
+    	  {  test: /\.m?js$/,
+	          exclude: /(node_modules|bower_components)/,
+	          use: {
+	            loader: 'babel-loader'
+	          }
+    	  }
+	    ]
+	  }
 };
